a621b8d5d1c782f504d3060717a0ba3d023781ee
BrRoman <editeur@traditions-monastiques.com>
2021-04-06 04:31:44 +0200

    Number of priests etc.

--- statistiques/apps/main/views.py
+++ statistiques/apps/main/views.py
@@ -2,16 +2,44 @@
 
 from django.shortcuts import render
 
-from .models import Moine
+from .models import Monk
 
 
 def home(request):
     """ Home view of statistiques. """
-    monks = Moine.objects.all().order_by('entry')
+    list_of_monks = Monk.objects.all().order_by('entry')
+    postulants = len(
+        Monk.objects
+        .filter(habit__isnull=True)
+    )
+    novices = len(
+        Monk.objects
+        .filter(temporary_profession__isnull=True)
+        .filter(habit__isnull=False)
+    )
+    tempo = len(
+        Monk.objects
+        .filter(perpetual_profession__isnull=True)
+        .filter(temporary_profession__isnull=False)
+    )
+    perpetual = len(
+        Monk.objects
+        .filter(perpetual_profession__isnull=False)
+    )
+    priests = len(
+        Monk.objects
+        .filter(ordination__isnull=False)
+    )
     return render(
         request,
         'main/home.html',
         {
-            'monks': monks,
+            'list_of_monks': list_of_monks,
+            'monks': len(list_of_monks),
+            'postulants': postulants,
+            'novices': novices,
+            'tempo': tempo,
+            'perpetual': perpetual,
+            'priests': priests,
         }
     )
